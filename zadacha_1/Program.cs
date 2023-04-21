1a88b583fe29845c3b7c5d6926f5b9c24b57df0e
Luydmila <Ludapanova178@gmail.com>
2023-04-21 15:38:49 +0500

    добавленно папки zadacha_1, zadacha_2, zadacha_3, файл .git ignor

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+
+
+double[,] a = new double[3, 4];
+
+Random random = new Random();
+
+    for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                a[i, j] = new Random().NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+                Write("{0,6:F2}", a[i, j]);
+            }
+            WriteLine();
+        }
+
+        double[,] FillMatrix(int rows, int cols)
+        {
+            double[,] array = new double[rows, cols];
+            Random rand = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    array[i, j] = Math.Round(rand.Next(-100, 101) * 0.1, 1);
+                }
+            }
+            return array;
+        }
+       void PrintArray(int[,] inArray)
+        {
+            for (int i = 0; i < inArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < inArray.GetLength(1); j++)
+                {
+                    Write($"{inArray[i, j]}");
+                }
+            }
+        }
+
---
+++ zadacha_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+Write("Введите количество строк массива: ");
+int m  = Convert.ToInt32(ReadLine());
+
+Write("Введите колиество сталбцов в массива: ");
+int n  = Convert.ToInt32(ReadLine());
+
+int[,] array = new int[m, n];
+
+
+PrintArray(array);
+
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Write(array[i, j] + "\t  ");
+        WriteLine();
+}
+
+ WriteLine("Введите числа: ");
+ int a = Convert.ToInt32(ReadLine());
+ int b = Convert.ToInt32(ReadLine());
+ if (a > m && b > n)
+    WriteLine("такого числа нет: ");
+ else
+if (a == m && b == n)
+    WriteLine("То такще число есть: ");
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]}");
+        }
+        WriteLine();
+    }
+}
